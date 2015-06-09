7601b30901b8a32f2f5b8ce2a767048640721948
idrozd <drozd.yuriy@gmail.com>
2015-06-10 01:43:00 +0300

    Added tip.

--- src/main/java/org/iasa/ui/AddController.java
+++ src/main/java/org/iasa/ui/AddController.java
@@ -1,10 +1,12 @@
 package org.iasa.ui;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.Writer;
@@ -108,7 +110,11 @@ public class AddController {
 
 	public List<String> getAllSteps() {
 		if (allSteps == null){
-			allSteps = JBehaveWrapper.getDefaultScenarios().stream().flatMap(sc -> sc.getSteps().stream()).collect(Collectors.toList());
+//			allSteps = JBehaveWrapper.getDefaultScenarios().stream().flatMap(sc -> sc.getSteps().stream()).collect(Collectors.toList());
+			BufferedReader reader = new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("tip.story")));
+			
+			allSteps = reader.lines().collect(Collectors.toList());		
+			
 		}
 		return allSteps;
 	}
---
+++ src/main/resources/tip.story
@@ -0,0 +1,26 @@
+Given user request site
+Given user is in system
+Given user is in game
+
+
+When user fill in the form with $login, $email, $password
+When user logs in with $email and $password
+When user goes to $field
+When search $name
+When goes to $field
+When run $game
+When add to favourites
+When remove from favourites
+When hearted the game
+When unhearted the game
+When add comment $comment
+
+
+Then verify message is $text
+Then verify $field is present
+Then verify $field is not present
+Then user log out
+Then choose $name
+Then verify found $game
+Then verify title is &title
+Then goes to My Profile
